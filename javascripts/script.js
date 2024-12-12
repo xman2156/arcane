115afc082b84cefd91775b789f48571695ba502c
Animation Hiatus <34029777+xman2156@users.noreply.github.com>
2024-12-12 15:56:11 -0600

    Updated for end of show

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,60 +1,7 @@
-//Automatic episode change
-var episodeList = [
-  ['The Monster You Created', '???', new Date("2021-11-20T00:00:00-05:00")]
-];
-var startDate = new Date("2022-03-19T10:00:00-05:00");
-var today = Date.now();
-var weeksPassed = 0; //Math.floor((today - Date.parse(startDate))/(24*3600*1000*7)); //comment out equation when on hiatus
-var latestRelease = episodeList[weeksPassed][2];
-var nextRelease = episodeList[weeksPassed][3];
-document.getElementById("previousEpisode").innerHTML = episodeList[weeksPassed][0];
-document.getElementById("nextEpisode").innerHTML = episodeList[weeksPassed][1];
-
+var latestRelease = new Date("2024-11-23T02:00:00-06:00");
 var oneDay = 24*60*60*1000;
 var mode = 0; //DD:HH:MM:SS mode is default
 var lastHiatusMention = null;
-
-function GetThen(yourUrl, onload){
-  var Httpreq = new XMLHttpRequest();
-  Httpreq.open("GET",yourUrl,true);
-  Httpreq.onload = function() {
-    if (Httpreq.readyState === Httpreq.DONE && Httpreq.status === 200) {
-      onload(Httpreq.responseText);
-    }
-  };
-  Httpreq.send(null);
-}
-
-//Initially loads the last 100 posts on subreddit
-function requestSubredditData(after = null) {
-  var url = 'https://www.reddit.com/r/arcane/new.json?limit=100';
-  GetThen(after ? url + '&after=' + after : url, checkSubreddit);
-}
-		
-//looks at the loaded posts, this runs four times every half-second
-function checkSubreddit(response){
-  var subbredditJSON = JSON.parse(response);
-  var lastHiatusMentionThisCheck;
-  //list of words that counts as a mention of the hiatus
-  var keywords = ["hiatus"];
-  for(var i = 0; i < 100; i++){
-    for(var j = 0; j < keywords.length; j++){
-      //checks only post titles and post content if self-post
-      if(subbredditJSON.data.children[i].data.selftext.toLowerCase().includes(keywords[j]) == true || subbredditJSON.data.children[i].data.title.toLowerCase().includes(keywords[j]) == true){
-        lastHiatusMentionThisCheck = new Date(subbredditJSON.data.children[i].data.created_utc * 1000);
-        document.getElementById("hiatusLink").href = "https://reddit.com" + subbredditJSON.data.children[i].data.permalink;
-        i = 100;
-      };
-    };
-  };
-  //loads the next 100 if hiatus is not mentioned then runs the function again
-  if (lastHiatusMentionThisCheck == null) {
-    requestSubredditData(subbredditJSON.data.after);
-  }
-  else {
-    lastHiatusMention = lastHiatusMentionThisCheck;
-  }
-};
 
 function switchMode(){
   if(mode == 0){
@@ -121,45 +68,9 @@ function timer(updown, zeroTime, id){
 //The Grand Array of Hiatuses
 var hiatusList = [
 ['Last Episode','Next Episode','Preceding Release','Following Release','Hiatus Length','Note'],
-['The Monster You Created','???','Nov 20 2021','Nov 2024',,'']
+['The Monster You Created','Heavy is the Crown','Nov 20 2021','Nov 9 2024',1085,''],
+['Heavy is the Crown','','Nov 9 2024','','','The end of Arcane']
 ];
-
-function hiatusRankCheck(){
-  var diffDays = timer("up", latestRelease, "count");
-  var hiatusRank = 0;
-  var nextHiatusLength = hiatusList[1][4]; //reference to the longest hiatus
-  for(var i = 1; i < hiatusList.length; i++){
-    if(hiatusList[i][4] > diffDays){
-      hiatusRank += 1;
-      if(hiatusList[i][4] < nextHiatusLength){
-        nextHiatusLength = hiatusList[i][4];
-      }
-    }
-  }
-  var suffix;
-  if(hiatusRank % 10 == 1 && hiatusRank != 11){
-    suffix = "st";
-  }
-  else if(hiatusRank % 10 == 2 && hiatusRank != 12){
-    suffix = "nd";
-  }
-  else if(hiatusRank % 10 == 3 && hiatusRank != 13){
-    suffix = "rd";
-  }
-  else if(hiatusRank == 0){
-    suffix = "The";
-  }
-  else suffix = "th";
-  if(hiatusRank > 0){
-    document.getElementById("hiatusRank").innerHTML =  hiatusRank + suffix;
-  }
-  else{
-    document.getElementById("hiatusRank").innerHTML =  suffix;
-  }
-  document.getElementById("nextHiatusLength").innerHTML =  nextHiatusLength;
-  var nextHiatusLengthDate = new Date(latestRelease.getTime() + (nextHiatusLength * 86400000));
-  return nextHiatusLengthDate;
-}
 
 //makes an HTML table from the array
 function createTable(array) {
@@ -181,10 +92,4 @@ function createTable(array) {
 //does the ticking
 window.setInterval(function(){
   timer("up", latestRelease, "count");
-  //timer("down", hiatusRankCheck(), "count2"); //comment out when not on hiatus
-  timer("up", lastHiatusMention, "count3");
-  timer("down", nextRelease, "count4");
 }, 250);
-
-//every 30 seconds, the most recent 100 posts on the subreddit are loaded up again in case there has been a new post that mentions hiatus
-window.setInterval(requestSubredditData, 30000);
